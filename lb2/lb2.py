9af7216b9420bd4fb8f017de2893b831ec53874d
Artanias <43622365+Artanias@users.noreply.github.com>
2022-12-12 23:09:16 +0300

    Added new example.

---
+++ lb2/lb2.py
@@ -0,0 +1,17 @@
+def mult(a, b):
+    return a * b
+
+def sub(a, b):
+    return a - b
+
+def div(a, b):
+    return a / b
+
+def sum(a, b):
+    return a + b
+
+
+mult(5, 10)
+sub(50, 15)
+div(10, 5)
+sum(6, 3)
